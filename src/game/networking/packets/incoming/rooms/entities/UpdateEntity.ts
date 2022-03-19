e872a0f26fb42c9a1fa40a98a7a4cec0062ed029
258ms <valeriotriolo3010@gmail.com>
2022-03-19 14:48:59 +0100

    big refactoring

--- src/game/networking/packets/incoming/rooms/entities/UpdateEntity.ts
+++ src/game/networking/packets/incoming/rooms/entities/UpdateEntity.ts
@@ -7,13 +7,14 @@ import Point3d from "../../../../../utils/point/Point3d";
 
 export default class UpdateEntity extends MessageHandler {
     public handle(): void {
-        let position = this.message.data;
+
+        let entity = this.message.data;
 
         // check, it's teleport?
 
         if(Engine.getInstance().RoomsManager?.CurrentRoom) {
-            let userVisualization = Engine.getInstance().RoomsManager?.CurrentRoom?.RoomUsersManager.getUser(parseInt(position.userId))?.Visualization as UserVisualization;
-            userVisualization.setPosition(new Point3d(position.x, position.y, position.z))
+            let userVisualization = Engine.getInstance().RoomsManager?.CurrentRoom?.RoomUsersManager.getUser(parseInt(entity.userId))?.Visualization as UserVisualization;
+            userVisualization.setPosition(new Point3d(entity.x, entity.y, entity.z))
             /*if(!userVisualization.IsWalking)
                 userVisualization.IsWalking = true;
 
