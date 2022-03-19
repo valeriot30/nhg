00b08a808edc00a635a806cd47d07da4887f797a
258ms <valeriotriolo3010@gmail.com>
2022-03-19 14:44:46 +0100

    Big refactoring

--- src/game/networking/packets/incoming/generic/DisconnectClient.ts
+++ src/game/networking/packets/incoming/generic/DisconnectClient.ts
@@ -6,10 +6,10 @@ import UIComponent from "../../../../engine/ui/components/UIComponentEnum";
 
 export default class DisconnectClient extends MessageHandler {
     public handle(): void {
-        let reason = this.message;
+        let reason = this.message.data;
 
         if(Engine.getInstance().RoomsManager?.CurrentRoom != null) {
-            (Engine.getInstance().RoomsManager?.CurrentRoom?.getRoomLayout().getVisualization() as RoomVisualization).Container.destroy();
+            (Engine.getInstance().RoomsManager?.CurrentRoom?.getRoomLayout().Visualization as RoomVisualization).Container.destroy();
         }
 
         (Engine.getInstance().getUserInterfaceManager().getUIComponentManager().getComponent(UIComponent.DefaultPanelUI) as DefaultPanel).set(reason);
